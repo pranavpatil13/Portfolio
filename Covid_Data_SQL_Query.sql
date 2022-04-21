6883b83515c1c8779d5d55892753ded4b7d99e0d
pranavpatil13 <93419523+pranavpatil13@users.noreply.github.com>
2022-04-21 22:37:05 +0530

    Update Covid_Data_SQL_Query.sql
    This contains a SQL query.

--- Covid_Data_SQL_Query.sql
+++ Covid_Data_SQL_Query.sql
@@ -121,7 +121,10 @@ select *
 from PercentPopulationVaccinated
 
 
-
+*****Citation*****
+Hannah Ritchie, Edouard Mathieu, Lucas Rodés-Guirao, Cameron Appel, Charlie Giattino, Esteban Ortiz-Ospina, Joe Hasell, Bobbie Macdonald, 
+Diana Beltekian and Max Roser (2020) - "Coronavirus Pandemic (COVID-19)". Published online at OurWorldInData.org. 
+Retrieved from: 'https://ourworldindata.org/coronavirus' [Online Resource]
 
 
 
